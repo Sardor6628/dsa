0a74d1873e941da70ac81bd0841bfd745be53612
Sardor6628 <sardor.dsu@gmail.com>
2024-07-03 14:42:14 +0900

    DSA:merge sort-simplified

--- sorting/s_4_merge_sort.py
+++ sorting/s_4_merge_sort.py
@@ -1,54 +1,79 @@
 import unittest
 
 
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    end = len(arr) - 1
-    start = 0
-    return mergeSort(arr, start, end)
+# def merge_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
+#     end = len(arr) - 1
+#     start = 0
+#     return mergeSort(arr, start, end)
+#
+#
+# def mergeSort(arr, start, end):
+#     if start >= end:
+#         return
+#     mid = start + (end - start) // 2
+#     mergeSort(arr, start, mid)
+#     mergeSort(arr, mid + 1, end)
+#     merge(arr, start, mid, end)
+#     return arr
+#
+#
+# def merge(arr, start, mid, end):
+#     leftArrayNum = mid - start + 1
+#     rightArrayNum = end - mid
+#     leftArray = [0] * leftArrayNum
+#     rightArray = [0] * rightArrayNum
+#     for i in range(leftArrayNum):
+#         leftArray[i] = arr[start + i]
+#     for i in range(rightArrayNum):
+#         rightArray[i] = arr[mid + 1 + i]
+#     indexOfRightArr = 0
+#     indexOfLeftArr = 0
+#     indexOfMergedArr = start
+#
+#     while indexOfRightArr < rightArrayNum and indexOfLeftArr < leftArrayNum:
+#         if leftArray[indexOfLeftArr] <= rightArray[indexOfRightArr]:
+#             arr[indexOfMergedArr] = leftArray[indexOfLeftArr]
+#             indexOfLeftArr += 1
+#         else:
+#             arr[indexOfMergedArr] = rightArray[indexOfRightArr]
+#             indexOfRightArr += 1
+#         indexOfMergedArr += 1
+#     while indexOfLeftArr < leftArrayNum:
+#         arr[indexOfMergedArr] = leftArray[indexOfLeftArr]
+#         indexOfLeftArr += 1
+#         indexOfMergedArr += 1
+#     while indexOfRightArr < rightArrayNum:
+#         arr[indexOfMergedArr] = rightArray[indexOfRightArr]
+#         indexOfMergedArr += 1
+#         indexOfRightArr += 1
+#     return arr
 
 
-def mergeSort(arr, start, end):
-    if start >= end:
-        return
-    mid = start + (end - start) // 2
-    mergeSort(arr, start, mid)
-    mergeSort(arr, mid + 1, end)
-    merge(arr, start, mid, end)
-    return arr
-
-
-def merge(arr, start, mid, end):
-    leftArrayNum = mid - start + 1
-    rightArrayNum = end - mid
-    leftArray = [0] * leftArrayNum
-    rightArray = [0] * rightArrayNum
-    for i in range(leftArrayNum):
-        leftArray[i] = arr[start + i]
-    for i in range(rightArrayNum):
-        rightArray[i] = arr[mid + 1 + i]
-    indexOfRightArr = 0
-    indexOfLeftArr = 0
-    indexOfMergedArr = start
-
-    while indexOfRightArr < rightArrayNum and indexOfLeftArr < leftArrayNum:
-        if leftArray[indexOfLeftArr] <= rightArray[indexOfRightArr]:
-            arr[indexOfMergedArr] = leftArray[indexOfLeftArr]
-            indexOfLeftArr += 1
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
         else:
-            arr[indexOfMergedArr] = rightArray[indexOfRightArr]
-            indexOfRightArr += 1
-        indexOfMergedArr += 1
-    while indexOfLeftArr < leftArrayNum:
-        arr[indexOfMergedArr] = leftArray[indexOfLeftArr]
-        indexOfLeftArr += 1
-        indexOfMergedArr += 1
-    while indexOfRightArr < rightArrayNum:
-        arr[indexOfMergedArr] = rightArray[indexOfRightArr]
-        indexOfMergedArr += 1
-        indexOfRightArr += 1
-    return arr
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+def merge_sort(arr):
+    n = len(arr)
+    if n <=1:
+        return arr
+    mid = n // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
 
 
 class TestMergeSort(unittest.TestCase):
